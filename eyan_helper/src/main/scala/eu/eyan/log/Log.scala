ca23bef3b37d81ada0d2aca398ea83e8af8ab521
githubeyaneu <github@eyan.eu>
2016-08-17 19:29:25 +0200

    Small changes

--- eyan_helper/src/main/scala/eu/eyan/log/Log.scala
+++ eyan_helper/src/main/scala/eu/eyan/log/Log.scala
@@ -11,11 +11,13 @@ object Log {
     LogWindow.add(stack(3).getClassName.substring(stack(3).getClassName.lastIndexOf(".") + 1) + "." + stack(3).getMethodName + ": " + message)
   }
   def error(message: String) = log(Error, message)
+  def error(exception: Throwable) = log(Error, exception.getMessage + "\r\n  " + exception.getStackTrace.mkString("  \r\n"))
   def warn(message: String) = log(Warn, message)
   def info(message: String) = log(Info, message)
   def debug(message: String) = log(Debug, message)
   def trace(message: String) = log(Trace, message)
 }
+class Log{}
 
 abstract class LogLevel {}
 case object Error extends LogLevel
