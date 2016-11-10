9ed462c8ee8298f9fc5f0a9197a3c9b6b9094674
Andras Frey <extern.andras.frey@elektrobit.com>
2016-11-10 13:49:41 +0100

    StringBuilder

--- eyan_helper/src/main/scala/eu/eyan/util/string/StringPlus.scala
+++ eyan_helper/src/main/scala/eu/eyan/util/string/StringPlus.scala
@@ -9,22 +9,24 @@ import eu.eyan.util.io.FilePlus.FilePlus
 
 object StringPlus {
   lazy val reg = "[\\p{InCombiningDiacriticalMarks}]".r
-  
-  def withoutAccents(s:String) = reg.replaceAllIn(Normalizer.normalize(s, Normalizer.Form.NFD), "").replaceAll("ß", "ss")
-  
-  implicit class StringPlus(val s: String){
-    def println = System.out.println(s)
-		def printlnErr = System.err.println(s)
 
-    def writeToFile(filename:String) = {
-    	val bw = new BufferedWriter(new FileWriter(filename))
-    	bw.write(s)
-    	bw.close
+  def withoutAccents(s: String) = reg.replaceAllIn(Normalizer.normalize(s, Normalizer.Form.NFD), "").replaceAll("ß", "ss")
+
+  implicit class StringPlus(val s: String) {
+    def println = System.out.println(s)
+    def printlnErr = System.err.println(s)
+
+    def writeToFile(filename: String): Unit = writeToFile(new File(filename))
+
+    def writeToFile(file: File): Unit = {
+      val bw = new BufferedWriter(new FileWriter(file))
+      bw.write(s)
+      bw.close
     }
-    
+
     def deleteAsFile = new File(s).delete
     def deleteAsDir = new File(s).deleteRecursively
-    
+
     def executeAsProcess = s.!!
   }
 }
