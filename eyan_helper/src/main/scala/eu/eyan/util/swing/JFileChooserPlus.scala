7821289eec93cad36f4da4045a6d74f5dbce5896
Andras Frey <extern.andras.frey@elektrobit.com>
2016-12-15 15:19:06 +0100

    JFileChooserPlus

---
+++ eyan_helper/src/main/scala/eu/eyan/util/swing/JFileChooserPlus.scala
@@ -0,0 +1,32 @@
+package eu.eyan.util.swing
+
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
+import java.awt.Component
+import java.io.File
+
+object JFileChooserPlus {
+
+  implicit class JFileChooserImplicit(fileChooser: JFileChooser) {
+
+	  def withCurrentDirectory(path: String) = { fileChooser.setCurrentDirectory(new File(path)); fileChooser }
+	  
+    def withDialogTitle(title: String) = { fileChooser.setDialogTitle(title); fileChooser }
+
+    def withApproveButtonText(text: String) = { fileChooser.setApproveButtonText(text); fileChooser }
+
+    def withFileFilter(extension: String, descriptionOfExtension: String) =
+      { fileChooser.setFileFilter(new FileNameExtensionFilter(descriptionOfExtension, extension)); fileChooser }
+
+    def showAndHandleResult(parent: Component, actionOnApprove: File => Unit, actionOnCancel: () => Unit = () => {}, actionOnError: () => Unit = () => {}) = {
+      val result = fileChooser.showOpenDialog(parent)
+      result match {
+        case JFileChooser.APPROVE_OPTION => actionOnApprove(fileChooser.getSelectedFile)
+        case JFileChooser.CANCEL_OPTION  => actionOnCancel()
+        case JFileChooser.ERROR_OPTION   => actionOnError()
+        case _                           => actionOnError()
+      }
+      this
+    }
+  }
+}
