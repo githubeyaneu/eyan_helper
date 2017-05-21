1f0b2116870215a6cec8fc563288ce56a1301885
eyan <git@eyan.eu>
2017-05-21 19:12:19 +0200

    FilePlus

--- eyan_helper/src/main/scala/eu/eyan/util/io/FilePlus.scala
+++ eyan_helper/src/main/scala/eu/eyan/util/io/FilePlus.scala
@@ -2,6 +2,8 @@ package eu.eyan.util.io
 
 import java.io.File
 import eu.eyan.util.string.StringPlus.StringPlusImplicit
+import java.security.MessageDigest
+import java.io.FileInputStream
 
 object FilePlus {
 
@@ -22,6 +24,24 @@ object FilePlus {
     def filesWithExtension(extension: String) = file.listFiles.filter(_.getName.endsWith("." + extension))
 
     def getFileTree: Stream[File] = FilePlus.getFileTree(file)
+
+    def hash = {
+			if (file.isFile()) {
+			  if(file.length > 50*1000*1000){
+				  val messageDigest = MessageDigest.getInstance("SHA")
+						  val is = new FileInputStream(file)
+				  val buffer = new Array[Byte](8192)
+				  is.skip(file.length/2)
+				  val bytesRead = is.read(buffer)
+				  if (bytesRead > 0) messageDigest.update(buffer, 0, bytesRead)
+				  val sha = messageDigest.digest
+				  
+				  sha.map("%02x".format(_)).mkString
+			  }
+			  else "small"
+			}
+			else "d"//throw new IllegalArgumentException("Create hash not possible to directory! "+file.getAbsolutePath)
+    }
   }
 
   def getFileTree(f: File): Stream[File] =
@@ -29,5 +49,5 @@ object FilePlus {
       if (f.isDirectory) f.listFiles().toStream.flatMap(getFileTree)
       else Stream.empty)
 
-  def fileTrees(paths: String*): Stream[File] = (paths map { _.asFile } map getFileTree ).toStream.flatten
+  def fileTrees(paths: String*): Stream[File] = (paths map { _.asFile } map getFileTree).toStream.flatten
 }
