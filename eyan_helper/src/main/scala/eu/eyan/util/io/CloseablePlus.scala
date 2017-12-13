76bb23de2d87e0c78121a503707a145e5c13730f
githubeyaneu <github@eyan.eu>
2017-12-13 16:10:46 +0100

    closeables

--- eyan_helper/src/main/scala/eu/eyan/util/io/CloseablePlus.scala
+++ eyan_helper/src/main/scala/eu/eyan/util/io/CloseablePlus.scala
@@ -2,12 +2,9 @@ package eu.eyan.util.io
 
 import java.io.Closeable
 import eu.eyan.log.Log
+import eu.eyan.util.scala.TryCatch
+import eu.eyan.util.scala.Try
 
 object CloseablePlus {
-  def closeQuietly(closeable: Closeable) =
-    try {
-      closeable.close()
-    } catch {
-      case e: Exception => Log.error(e)
-    }
+  def closeQuietly(closeable: Closeable) = Try( if(closeable!=null) closeable.close else Log.error("closeable null"))
 }
--- eyan_helper/src/main/scala/eu/eyan/util/scala/Try.scala
+++ eyan_helper/src/main/scala/eu/eyan/util/scala/Try.scala
@@ -4,12 +4,13 @@ import scala.util.Failure
 import scala.util.Success
 import java.io.Closeable
 import eu.eyan.log.Log
+import eu.eyan.util.io.CloseablePlus
 
 object Try { def apply[T](action: => T): T = TryFinally[T](action, {}) }
 object TryCatch { def apply[T](action: => T, errorAction: Throwable => T): T = TryCatchFinally[T](action, errorAction, {}) }
 object TryFinally { def apply[T](action: => T, finaly: => Unit): T = TryCatchFinally[T](action, e => { Log.error(e); null.asInstanceOf[T] }, finaly) } //TODO / FIXME: in error case null as result. Is it good? Should be Optional?
 object CloseFinally { def apply[T, CLOSEABLE <: Closeable](closeable: CLOSEABLE, action: CLOSEABLE => T): T = FinallyClose(closeable, action) }
-object FinallyClose { def apply[T, CLOSEABLE <: Closeable](closeable: CLOSEABLE, action: CLOSEABLE => T): T = TryCatchFinally(action(closeable), e => { Log.error(e); null.asInstanceOf[T] }, closeable.close) }
+object FinallyClose { def apply[T, CLOSEABLE <: Closeable](closeable: CLOSEABLE, action: CLOSEABLE => T): T = TryCatchFinally(action(closeable), e => { Log.error(e); null.asInstanceOf[T] }, CloseablePlus.closeQuietly(closeable)) }
 object TryCatchFinally { def apply[T](action: => T, errorAction: Throwable => T, finaly: => Unit): T = new Try[T](action, finaly).Catch(errorAction) }
 
 private class Try[T](action: => T, finaly: => Unit) {
