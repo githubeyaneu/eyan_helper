d9fc8c6a1a3a588465c96b19d7fe165ff0d00205
anfr895 <anfr895@DEL00988.ebgroup.elektrobit.com>
2019-03-15 16:20:59 +0100

    TP+

--- eyan_helper/src/main/scala/eu/eyan/util/java/lang/ThreadPlus.scala
+++ eyan_helper/src/main/scala/eu/eyan/util/java/lang/ThreadPlus.scala
@@ -10,17 +10,13 @@ object ThreadPlus {
 
   def run[T](action: => T) = new ThreadRunner(action)
   class ThreadRunner[T](action: => T) {
-    val done$ = BehaviorSubject[Boolean](false)
+    var done = false
     private var ret: Option[T] = None
-    val thread = new Thread(RunnablePlus.runnable({
+    new Thread(RunnablePlus.runnable({
       ret = try { Option(action) } catch { case e: Throwable => { Log.error(e); None } }
-      done$.onNext(true)
-      done$.onCompleted
-    }))
-    thread.start
+      done = true
+    })).start
     def result: Option[T] = ret
-    def stop = {thread.stop; done$.onNext(true);  done$.onCompleted}
-
   }
 
   //  def runSafe[T](action: => T) = new ThreadRunnerSafe(action).result.distinctUntilChanged
